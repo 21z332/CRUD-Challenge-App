d0e4ef3f151a3165bd2d3abfe0d25cb41a532ac6
Prathibha Gurusamy <95732448+21z332@users.noreply.github.com>
2024-08-08 10:00:52 +0530

    Add files via upload

---
+++ ChallengeApp/src/test/java/com/springbasicproject/ChallengeApp/ChallengeAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.springbasicproject.ChallengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
